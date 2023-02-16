6cc187f7a30b8d30df14bc218cf8e7495cb1301a
Niloy <dev.niloysarkar@gmail.com>
2023-02-16 12:00:26 +0600

    Submissions Bug Fixed

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -14,6 +14,7 @@ import AllMailsFragment from "../../components/DashboardComponents/Fragments/All
 import TrashFragment from "../../components/DashboardComponents/Fragments/TrashFragment"
 import SpamFragment from "../../components/DashboardComponents/Fragments/SpamFragment"
 import axios from "axios"
+import MyCircularProgress from "../../components/DashboardComponents/MyCircularProgress"
 
 const Dashboard = () => {
     const router = useRouter()
@@ -47,6 +48,9 @@ const Dashboard = () => {
     useEffect(() => {
         getUser()
     }, [user])
+
+    if(!Boolean(user) || !Boolean(user.first_name)) return <MyCircularProgress/>
+    
     return (
         <Box>
             {/* Do not render these componenets if the user is null */}
