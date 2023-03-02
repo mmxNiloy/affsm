1cad8e06bd49bc956875d74a99566535c95cdd4c
AyeshaSareena <ayeshasareen48@gmail.com>
2023-03-02 11:43:38 +0600

    Downloadable Admit Card

--- components/DashboardComponents/PDF/MyPDFViewer.js
+++ components/DashboardComponents/PDF/MyPDFViewer.js
@@ -3,11 +3,24 @@ import { useEffect, useState } from "react"
 import EmptyList from "../EmptyList"
 import MyDocument from "./MyDocument"
 
-const MyPDFViewer = ({data}) => {
+const MyPDFViewer = ({data, admitCard}) => {
     
     useEffect(() => {}, [])
     
     if(!Boolean(data)) return <EmptyList/>
+
+    if(admitCard) 
+        return (
+            <PDFViewer style={{ 
+                display: 'flex',
+                alignSelf: 'center',
+                justifySelf: 'center',
+                width: '100vw', 
+                height: '100vh',
+                }}>
+                <MyDocument data={data} admitCard/>
+            </PDFViewer>
+        )
     
     return (
         <PDFViewer style={{ 
