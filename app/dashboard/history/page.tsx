619e5c51411f8f03c6a1ad0cdbd2a0bd49957747
Niloy Sarkar <dev.niloysarkar@gmail.com>
2024-10-10 00:34:47 +0600

    Removed history page

--- app/dashboard/history/page.tsx
+++
@@ -1,6 +0,0 @@
-import SubmissionsTab from "@/app/components/DashboardComponents/Tabs/SubmissionsTab/SubmissionsTab";
-import React from "react";
-
-export default function HistoryPage() {
-  return <SubmissionsTab />;
-}
